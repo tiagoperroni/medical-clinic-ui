0c69b9c424cc1e1f414d6a6c6dfb0470e3870791
Tiago <tiagoperroni86@gmail.com>
2023-01-25 21:31:55 -0300

    starting project with entities modules and routings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppointmentModule } from './appointment/appointment/appointment.module';
+import { DoctorModule } from './doctor/doctor/doctor.module';
+import { PatientModule } from './patient/patient/patient.module';
+import { RoomModule } from './room/room/room.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, DoctorModule, RoomModule, PatientModule, AppointmentModule
   ],
   providers: [],
   bootstrap: [AppComponent]
