0b0e0ba53274f57cc0772fcce2b63bdb030f0fb7
Tiago <tiagoperroni86@gmail.com>
2023-01-26 06:20:36 -0300

    add material for

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,23 @@ import { AppointmentModule } from './appointment/appointment/appointment.module'
 import { DoctorModule } from './doctor/doctor/doctor.module';
 import { PatientModule } from './patient/patient/patient.module';
 import { RoomModule } from './room/room/room.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule, DoctorModule, RoomModule, PatientModule, AppointmentModule
+    BrowserModule,
+    DoctorModule,
+    RoomModule,
+    PatientModule,
+    AppointmentModule,
+    BrowserAnimationsModule,
+    MatSlideToggleModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
